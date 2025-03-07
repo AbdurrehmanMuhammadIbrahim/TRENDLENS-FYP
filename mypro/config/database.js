fea58ee20eee612a3a39cfac541df9133b3c2047
Abdurrehman <education24to7@gmail.com>
2025-03-08 03:40:51 +0500

    Update database.js

--- mypro/config/database.js
+++ mypro/config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'fyp_database'),
+      database: env('DATABASE_NAME', 'your database'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'Disc765!@'),
+      password: env('DATABASE_PASSWORD', 'your password'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   }, 
